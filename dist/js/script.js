d94e88ee26dd0532cab492296cf33b279a9488e4
Pavel <whatisclamp@gmail.com>
2021-01-18 21:41:19 +0200

    Add slider

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,30 @@
+const slider = tns({
+    container: '.feature__inner',
+    items: 5,
+    slideBy: 1,
+    autoplay: false,
+    controls: false,
+    nav: false,
+    navPosition: 'bottom',
+    speed: 1000,
+    loop: true,
+    responsive: {
+        1: {
+            nav: true   
+        },
+        1200: {
+            nav: false
+        }
+    }
+  });
 
-    // $(function(){
-    //     $("#datepicker").datepicker();
-    // });
+  document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+  });
+
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+  });
 
 $(document).ready(function(){
     $( function() {
@@ -16,9 +39,4 @@ $(document).ready(function(){
     });
 
     $('input.timepicker').timepicker({});
-    $(".owl-carousel").owlCarousel({
-        loop: true,
-        center: true,
-        autoWidth: true
-    });
 });
